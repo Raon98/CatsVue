02049bb468fa0bbad63d849f9751a179e752331b
Raon98 <cheol1998@kakao.com>
2022-05-30 15:13:44 +0900

    inputdata

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,6 +6,9 @@
     <li v-for="(item,i) in list" :key="i">{{item}}</li>
   </ol>
   <button @click="handleClick">Click</button>
+  <div CLASS="lines">{{inputdata}}</div>
+  <input type="text" v-model="inputdata">
+
 </template>
 
 <script>
@@ -14,21 +17,21 @@ import { ref} from "vue";
 const useVales = () => {
       const message = ref('hello Vue.js');
       const list = ref(['사과','바나나','딸기']);
-
-      return { message , list }
+      const inputdata = ref("");
+      return { message , list , inputdata}
     }
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Main",
   setup() {
-    const {message, list} = useVales()
+    const {message, list, inputdata} = useVales()
 
     const handleClick = (event) => {
       alert(event.target)
       list.value.push('오렌지')
     }
 
-    return {message, list, handleClick}
+    return {message, list, handleClick, inputdata}
   }
 }
 
@@ -37,5 +40,15 @@ export default {
 </script>
 
 <style scoped>
+.lines{
+  line-height: 20px;
+  border-collapse: collapse;
+  border: 1px solid black;
+  margin: 15px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+p {
 
+}
 </style>
