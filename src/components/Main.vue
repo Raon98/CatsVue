e0ac234150b6d92904770f36e384e9004969b384
Raon98 <cheol1998@kakao.com>
2022-05-30 14:53:56 +0900

    refectoring

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,26 +1,39 @@
 <template>
-  {{message}}
+  <div>
+  <p>{{message}}</p>
+  </div>
   <ol>
     <li v-for="(item,i) in list" :key="i">{{item}}</li>
   </ol>
+  <button @click="handleClick">Click</button>
 </template>
 
 <script>
+import { ref} from "vue";
+
+const useVales = () => {
+      const message = ref('hello Vue.js');
+      const list = ref(['사과','바나나','딸기']);
+
+      return { message , list }
+    }
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Main",
-  data() {
-    return{
-      message : 'hello Vue.js',
-      list:['사과','바나나','딸기'],
-    }},
-  methods:{
+  setup() {
+    const {message, list} = useVales()
 
-  },
-  computed:{
+    const handleClick = (event) => {
+      alert(event.target)
+      list.value.push('오렌지')
+    }
 
+    return {message, list, handleClick}
   }
 }
+
+
+
 </script>
 
 <style scoped>
