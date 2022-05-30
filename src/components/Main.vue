5d5ad6e4bb1715ac063ca34cdae6d78bdcd624d8
Raon98 <cheol1998@kakao.com>
2022-05-30 14:34:46 +0900

    v -for 추가

---
+++ src/components/Main.vue
@@ -0,0 +1,28 @@
+<template>
+  {{message}}
+  <ol>
+    <li v-for="(item,i) in list" :key="i">{{item}}</li>
+  </ol>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Main",
+  data() {
+    return{
+      message : 'hello Vue.js',
+      list:['사과','바나나','딸기'],
+    }},
+  methods:{
+
+  },
+  computed:{
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
