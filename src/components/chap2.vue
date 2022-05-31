574cc814087c694002b2e98a4ab4b0e8d8ee5e1b
Raon98 <cheol1998@kakao.com>
2022-05-31 09:49:16 +0900

    count

--- src/components/chap2.vue
+++ src/components/chap2.vue
@@ -1,11 +1,43 @@
 <template>
-<div> test</div>
+<div :text-content.prop="message"> hi</div>
+<div :scroll-top.prop="scroll"></div>
+<div>
+  <div>
+    <p>{{count}}</p>
+    <button @click="increment"> 카운트 하기</button>
+  </div>
+</div>
 </template>
 
 <script>
+
+import {ref} from "vue";
+
+const chap2  = () => {
+  const  message = ref('hello Vue.js');
+  const scroll = ref(0);
+
+  return { message , scroll }
+}
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
-  name: "chap2"
+  name: "chap2",
+  data: () =>({
+      count : 0
+  }),
+  setup(){
+    // eslint-disable-next-line no-unused-vars
+    const { message, scroll } = chap2()
+    return { message , scroll  }
+
+  },
+  methods :{
+    increment() {
+      this.count = this.count += 1
+    }
+
+  }
+
 }
 </script>
 
