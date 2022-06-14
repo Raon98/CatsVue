26e96a376e1428d862f8f789be37fc2acc20d956
Raon98 <cheol1998@kakao.com>
2022-06-14 17:22:38 +0900

    watch

--- src/components/chap6.vue
+++ src/components/chap6.vue
@@ -1,24 +1,31 @@
 <template>
   <div>
-    <button @click="sibling">데이터 전송</button>
+    <button @click="sibling">배열 데이터 전송</button>
+    <button @click="sibling2">오브젝트 데이터 전송</button>
   </div>
 </template>
 
 <script>
 
-import {inject} from "vue";
+import {inject,ref} from "vue";
 
 export default {
+
   // eslint-disable-next-line vue/multi-word-component-names
   name: "chap6",
   setup(){
+    const check = ref(true);
     const emitter = inject('emitter')
 
     const sibling = () => {
           emitter.emit('sibling', ["sungcheol","chaemin"])
     }
-
-      return {sibling}
+    const sibling2 = () => {
+          emitter.emit('sibling2', {men : {spName: "sungcheol" , nic : "laon" , check : true},
+                                    girl : {spName: "cheamin" , nic : "hannah" ,check : false
+            }})
+    }
+      return {sibling, sibling2,check}
   }
 }
 </script>
